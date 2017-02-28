e42020f051194e56163c7c18a7e07d18f0d95c59
Quincy <QClarke@macbook-pro-0385-quincy-clarke.local>
2017-02-28 13:41:17 -0500

    Initial Commit

---
+++ Pinball ISP/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Pinball ISP
+//
+//  Created by Quincy on 2017-02-28.
+//  Copyright © 2017 Quincy. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
