bb6c9db7f31d18cef3ea9165ed08b7dc02bab02b
Quincy <QClarke@macbook-pro-0385-quincy-clarke.local>
2017-03-31 14:29:51 -0400

    Made sure that all the sprites are in front of the background by declaring the z position

--- Pinball ISP/GameScene.swift
+++ Pinball ISP/GameScene.swift
@@ -19,14 +19,29 @@ class GameScene: SKScene {
         
         background.position = CGPoint(x: size.width/2, y: size.height/2) //set it in the middle of the screen
         background.size = self.frame.size //set backgound to be the same size of the frame
+        background.zPosition = -1
         addChild(background)
         
+        //adding green bumpers
         let leftbumper = SKSpriteNode(imageNamed: "left bumper")
         leftbumper.position = CGPoint(x: 180, y: 415)
+        background.zPosition = 1
         addChild(leftbumper)
         
         let rightbumper = SKSpriteNode(imageNamed: "Right bottom bumper")
         rightbumper.position = CGPoint(x: 450, y: 415)
+        background.zPosition = 1
         addChild(rightbumper)
+        
+        //adding the bumpers that hit the ball
+        let leftpaddle = SKSpriteNode(imageNamed: "left trigger")
+        leftpaddle.position = CGPoint(x: size.width/2 - 70, y: 315)
+        background.zPosition = 1
+        addChild(leftpaddle)
+        
+        let rightpaddle = SKSpriteNode(imageNamed: "right trigger")
+        rightpaddle.position = CGPoint(x: size.width/2 - 10, y: 315)
+        background.zPosition = 1
+        addChild(rightpaddle)
     }
 }
